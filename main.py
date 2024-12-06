6e27c62cae9b82130e3ab0d7209c273dc6d58764
Sandeep Dhaliwal <104098660+Pbjsd@users.noreply.github.com>
2024-12-06 03:17:23 +0000

    Data Visualization: added the graph with some of the data components

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 filename = 'data.csv'
 
@@ -12,3 +13,12 @@ with open(filename, 'r') as csvfile:
     print(continent)
     print(year)
     print(population)
+
+plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker="o")
+plt.plot([2000, 2010, 2020, 2030], [200, 300, 500, 800], label="Asia", marker="o")
+plt.title("Internet Population per Continent")
+plt.xlabel("Year")
+plt.ylabel("Internet Users")
+plt.grid(True)
+plt.legend()
+plt.show()

507a5161ef7e9b988488846fb64739148db5d21c
Sandeep Dhaliwal <104098660+Pbjsd@users.noreply.github.com>
2024-12-12 19:00:34 +0000

    Connected the data dots

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ with open(filename, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
   for line in reader: 
     continent = line['continent']
-    year = line['year']
-    population = line['population']
+    year = int(line['year'])
+    population = int(line['population'])
 
     if continent not in population_per_continent:
       population_per_continent[continent] = {'population': [], 'years': []}
@@ -19,12 +19,16 @@ with open(filename, 'r') as csvfile:
     population_per_continent[continent]['years'].append(year)
 
 
+for continent in population_per_continent: 
+  years = population_per_continent[continent]['years']
+  population = population_per_continent[continent]['population']
+  plt.plot(years, population, label=continent, marker="o", alpha=0.5)
 
-plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker="o")
-plt.plot([2000, 2010, 2020, 2030], [200, 300, 500, 800], label="Asia", marker="o")
 plt.title("Internet Population per Continent")
 plt.xlabel("Year")
-plt.ylabel("Internet Users")
+plt.ylabel("Internet users (in billion users)")
 plt.grid(True)
+plt.tight_layout()
 plt.legend()
+
 plt.show()
